1fa2faa3cd1ac065244a12195b7073c12cd7ce7c
mikail <92223514+mikailsss@users.noreply.github.com>
2025-03-25 20:55:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from AsyncPayments.lolz import AsyncLolzteamMarketPayment
 from AsyncPayments.cryptoBot import AsyncCryptoBot
 BOT_TOKEN='7485758272:AAEV1aSJM3hcq685lO26YT79SP1Cz8tmTjM' # апи токен бота
 ADMIN_LIST = [1166639026] # список айди админов без кавычек через запятую
-CRYPTO_BOT_TOKEN = AsyncCryptoBot(token='351685:AA7zc9jQol6mcF1pdZP4bsZatduoxNJPRGC', is_testnet=False)
+CRYPTO_BOT_TOKEN = AsyncCryptoBot(token='', is_testnet=False)
 LOLZ_TOKEN = '123123' #введи свой лолз токен здесь
 SUPPORT_LINK = 't.me/sc4m3p'# ссылка на тп
 USD_RUB_COURSE = 85 # 1 USDT = 85 RUB
